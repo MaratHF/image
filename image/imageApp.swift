d04096731730f4ff27afece43df93ea27077f1f4
Marat Khayrullin <m.f.khayrullin@gmail.com>
2022-05-31 11:57:31 +0300

    Initial Commit

---
+++ image/imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageApp.swift
+//  image
+//
+//  Created by MAC  on 31.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
